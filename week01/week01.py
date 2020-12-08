1f519214540eedfff9df3ec63b42eefd5f7639ae
Dang Wengang <dangwg@126.com>
2020-12-08 15:43:36 +0800

    Push week01. 2

--- week01/week01.py
+++ week01/week01.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+"""
+    # README
+    编写一个函数, 当函数被调用时，将调用的时间记录在日志中, 日志文件的保存位置建议为：/var/log/python- 当前日期 /xxxx.log
+"""
+
 import os
 import sys
 import logging
